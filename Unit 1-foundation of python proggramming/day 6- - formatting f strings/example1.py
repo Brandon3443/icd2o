edec0004461d8edb69bc830dcc6df9cac694db10
Brandon Clarke <BClarke@bayviewglen.ca>
2023-10-02 14:18:37 -0400

    example1

---
+++ Unit 1-foundation of python proggramming/day 6- - formatting f strings/example1.py
@@ -0,0 +1,15 @@
+average=12.43576347563456
+print(f"{average}")
+print(f"{average: .2f}")
+print(f"{average: .3f}")
+
+number = 6.1
+print(f"{number: .2f}")
+
+number = 6
+print(f"{average: .2f}")
+
+math_mark = 34
+math_total = 45
+
+math_average = math_mark / math_total
