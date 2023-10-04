1e96ee8e90aa57afd6742e6aa4e7486f360cfc9a
Brandon Clarke <BClarke@bayviewglen.ca>
2023-10-04 13:55:08 -0400

    day 6 hw

---
+++ Unit 1-foundation of python proggramming/day 6- - formatting f strings/table.py
@@ -0,0 +1,26 @@
+item = "Product"
+
+ 
+
+quantity = 3
+
+ 
+
+price = 25.99
+
+ 
+
+total = price * quantity
+
+ 
+
+ 
+
+ 
+
+print(f"{'Item':>5} {'Quantity':>15} {'Price':>16.5} {'Total':>25}")
+
+ 
+
+print(f"{item:>5} {quantity:>10} {price:>20.2f} {total:>25}")
+
