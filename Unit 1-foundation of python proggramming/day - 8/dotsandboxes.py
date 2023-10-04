35c1300556550de6f6cc61a82692f664217956cf
Brandon Clarke <BClarke@bayviewglen.ca>
2023-10-04 13:54:52 -0400

    dots and boxes project

---
+++ Unit 1-foundation of python proggramming/day - 8/dotsandboxes.py
@@ -0,0 +1,27 @@
+print("Game #1")
+print("Opponent's name: ishaan")
+print("Opponent's Points: 30")
+print("your point's: 6")
+
+print("Game #2")
+print("Opponent's name: Kamea")
+print("Opponent's Points: 17")
+print("your point: 19")
+
+print("Game #3")
+print("Opponent's name: ketan")
+print("Opponent's Points: 20")
+print("your point's: 16")
+
+print("Game #4")
+print("Opponent's name: Abe")
+print("Opponent's Points: 13")
+print("your point's: 23")
+
+print("Game #5")
+print("Opponent's name: Alex")
+print("Opponent's Points: 18")
+print("your point's: 18")
+
+print(f"{} {} {} {}")
+
