c61b550096a6fddbe5d138247f72f20c512a34f2
Brandon Clarke <BClarke@bayviewglen.ca>
2023-10-04 20:21:03 -0400

    dots and boxes project

--- Unit 1-foundation of python proggramming/day - 8/dotsandboxes.py
+++ Unit 1-foundation of python proggramming/day - 8/dotsandboxes.py
@@ -1,27 +1,54 @@
+print(str(input("enter your name")))
+print("")
 print("Game #1")
 print("Opponent's name: ishaan")
 print("Opponent's Points: 30")
 print("your point's: 6")
-
+name1 = "ishaan"
+opoints1 = 30
+mpoints1 = 6
+print("")
 print("Game #2")
 print("Opponent's name: Kamea")
 print("Opponent's Points: 17")
 print("your point: 19")
-
+name2 = "kamea"
+opoints2 = 17
+mpoints2 = 19
+print("")
 print("Game #3")
 print("Opponent's name: ketan")
 print("Opponent's Points: 20")
 print("your point's: 16")
-
+name3 = "ketan"
+opoints3 = 20
+mpoints3 = 16
+print("")
 print("Game #4")
 print("Opponent's name: Abe")
 print("Opponent's Points: 13")
 print("your point's: 23")
-
+name4 = "Abe"
+opoints4 = 13
+mpoints4 = 23
+print("")
 print("Game #5")
 print("Opponent's name: Alex")
 print("Opponent's Points: 18")
 print("your point's: 18")
+name5 = "Alex"
+opoints5 = 18
+mpoints5 = 18
 
-print(f"{} {} {} {}")
-
+box1 = 6/36 *100
+box2 = 19/36 * 100
+box3 = 16/36 * 100
+box4 = 23/36 * 100
+box5 = 18/36 * 100
+print("")
+print(f"{'Opponent' :>7} {'My points' :>14} {'Oponnents points' :>21} {'Box %' :>9}")
+print(f"{name1 :>7} {mpoints1 :>11} {opoints1 :>17} {box1 :>30}")
+print(f"{name2 :>6} {mpoints2 :>12} {opoints2 :>17} {box2 :>29}")
+print(f"{name3 :>6} {mpoints3 :>12} {opoints3 :>17} {box3 :>29}")
+print(f"{name4 :>4} {mpoints4 :>14} {opoints4 :>17} {box4 :>30}")
+print(f"{name5 :>5} {mpoints5 :>13} {opoints5 :>17} {box5 :>16}")
