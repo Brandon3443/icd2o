2744350023ba7c63d071c4bc7aa8bf64eed7a994
Brandon Clarke <BClarke@bayviewglen.ca>
2023-10-25 22:12:26 -0400

    day 3 homework

--- unit 2 - wiriting python functions and variable scope/day one introduction to reusable code/day 3 writing functions/homework.py
+++ unit 2 - wiriting python functions and variable scope/day one introduction to reusable code/day 3 writing functions/homework.py
@@ -1,40 +1,45 @@
 def calculate_circle_area(radius):
-   print(f"the area of the circle is: {radius}")
-radius2 = int(input("Please enter radius here: "))
+  print(f"the are of the circle is {radius}")
+radius2 = float(input("enter the radius: "))
 radius = (3.14*radius2)**2
 calculate_circle_area(radius)
-##################################
-def calculate_rectange_area(length, width):
-    print(f"the area of the rectangle is: {area}")
-length = int(input("Please enter length here: "))
-width = int(input("Please enter width here: "))
-area = length * width
-calculate_rectange_area(length, width)
-##################################
+
+def calculate_rectangle_area(length, width):
+  print(f"the area of the rectangle is {area}")
+length=float(input("enter the legnth"))
+width=float(input("enter the width"))
+area=length*width
+calculate_rectangle_area(length, width)
+
 def calculate_cylinder_volume(radius3, height):
     print(f"the volume of the cylinder is: {volume}")
 radius3 = int(input("Please enter radius here: "))
 height = int(input("Please enter height here: "))
 volume = ((3.14*radius3)**2)*height
 calculate_cylinder_volume(radius3, height)
-##################################
+
 def greet_person(greeting):
-    print(f"Hello, {first_name} {last_name}!")
-first_name = str(input("Please enter your first name here: "))
-last_name = str(input("Please enter your last name here: "))
+  print(f"hello, {greeting}!")
+first_name = str(input("what is your first name: "))
+last_name = str(input("what is your last name: "))
 greeting = first_name + last_name
 greet_person(greeting)
-##################################
-def format_sales(price):
-    print(f"You purchased {quantity} {product} for a total of ${price}.")
-quantity = int(input("Please enter the quantity amount here: "))
-product = str(input("Please enter the product name here: "))
-price2 = float(input("Please enter the price rounded to 2 decimals here: "))
-price = price2 * quantity
-format_sales(price)
-##################################
-def calculate_bmi(weight):
-    print(f"Your weight is: {weight}lbs.")
-weight = float(input("Please enter your weight with 2 decimal places here: "))
-calculate_bmi(weight)
-##################################
+
+def calculate_bmi(weight, height):
+  print(f"your BMI is {BMI :.2f}")
+weight = float(input("what is your weight in kg: "))
+height = float(input("what is your height in meters: "))
+BMI = weight/height**2
+calculate_bmi(weight, height)
+
+def format_sales(product_name, price, quantity):
+  print(f"you purchased {quantity} {product_name}, the total price was ${price :.2f}")
+product_name = str(input(" What item did you buy "))
+quantity = int(input("how many did you buy "))
+price = float(input("what was the total price "))
+format_sales(product_name, price, quantity)
+
+
+
+
+
