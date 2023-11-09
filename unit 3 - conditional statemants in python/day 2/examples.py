d07f0e77279c282e1f4053784b0151382122e7ef
Brandon Clarke <BClarke@bayviewglen.ca>
2023-11-09 12:39:25 -0500

    day2 examples

---
+++ unit 3 - conditional statemants in python/day 2/examples.py
@@ -0,0 +1,57 @@
+number_grade = 76
+
+#if number_grade >= 80:
+    #print("A")
+#if number_grade >= 70:
+    #print("B")
+#if number_grade >= 60:
+   # print("c")
+#if number_grade >= 50:
+  #  print("D")
+#if number_grade < 50:
+ #   print("F")
+
+#number_grade = int(input("Ishaan what was ur score "))
+
+#if number_grade >= 80:
+  #  print("A")
+#if 70 <= number_grade < 80:
+ #   print("B")
+#if 60 <= number_grade < 70:
+ #   print("c")
+#if 50 <= number_grade < 60:
+ #   print("D")
+#if number_grade < 50:
+#    print("F")
+
+#if number_grade >= 80:
+ #   print("A")
+#elif number_grade >= 70:
+ #   print("B")
+#elif number_grade >= 60:
+ #   print("c")
+#elif number_grade >= 50:
+#    print("D")
+#elif number_grade < 50:
+ #   print("F")
+
+if number_grade >= 80:
+    print("A")
+elif number_grade >= 70:
+    print("B")
+elif number_grade >= 60:
+    print("c")
+elif number_grade >= 50:
+    print("D")
+else:
+    print("F")
+
+
+x = int(input("Ishaan please enter the value of X "))
+def inspect_number(x):
+    if x > 0:
+        print ("positive")
+    elif x < 0:
+        print ("Negative")
+    else:
+        print("Zero")
