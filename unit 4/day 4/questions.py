1683519fbb31ae59404af7c4b86220d7bb95bae1
Brandon Clarke <BClarke@bayviewglen.ca>
2023-12-14 08:36:00 -0500

    day 4 quiz questions

---
+++ unit 4/day 4/questions.py
@@ -0,0 +1,86 @@
+#Q1
+#for i in range(10):
+#    i +=1
+ #   print(i)
+
+#q2
+
+#for i in range(2,11,2):
+#    print(i)
+
+#q3
+#sum = 0
+#for i in range(1,8,2):
+#    sum += i
+#print(sum)
+
+#q4
+#print the numbers from 10 - 1 using range with a negative step
+#for i in range(10, 0, -1):
+#    print(i)
+
+#q5
+#for i in range(4, 21, 4):
+#    print(i)
+
+#q6
+#product=1
+#for i in range(1,6):
+#    product*= i
+#print(product)
+
+#q7
+#for i in range(3,13):
+#    if i % 3 == 0:
+#        print(i)
+
+#q8
+#for i in range(5,0,-1):
+#    print(i)
+#print("Blast Off!")   
+
+#q9
+#import math
+#for i in range(1,6):
+#    print(math.pow(i,2))
+
+#q10
+#def factorial(n)
+#    for n in range(1,8):
+#        product = 1
+#        for i in range (n,0,-1):
+#            product *= i
+#        return product
+#for n in range(1,8):
+#    print(factorial(n))
+
+#q11
+#str = "WONDERFUL"
+#result = ""
+#for i in range(1,len(str),2):
+#    result += str[i]
+#print(result)
+
+#q12
+#did it
+
+#q13
+#str = "COMPUTER".lower()
+#
+#count = 0
+#for i in range(len(str)):
+#    if str [i] in ('a','e','i','o','u'):
+#        count += 1
+#print(count)
+
+#q14
+#backwards = ""
+#forwards = "RACECAR"
+#
+#for i in range(len(forwards)-1,-1,-1):
+#    backwards += forwards[i]
+#
+#if backwards == forwards:
+#    print("it is a planidrome")
+#else:
+#    print("it is not a plaindrome")
