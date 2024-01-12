c032a296ef9ef39e9d28018195a57b9cc39eca62
Brandon Clarke <BClarke@bayviewglen.ca>
2024-01-12 13:51:16 -0500

    stuff i forgot to push

--- unit 2 - wiriting python functions and variable scope/day 5/bugtype.py
+++ unit 2 - wiriting python functions and variable scope/day 5/bugtype.py
@@ -1,4 +1,4 @@
-def input_bug_counts(bug_type):
+def input_bug_count(bug_type):
     count = int(input(f"Enter the count of {bug_type} bugs: "))
     return count
 
@@ -10,7 +10,7 @@ def calculate_percent(total, count):
 
 def input_bug_type_and_count():
     bug_type = input("Enter the type of bug: ")
-    count = input_bug_counts(bug_type)
+    count = input_bug_count(bug_type)
     return bug_type, count
 
 
@@ -18,9 +18,9 @@ def display_table(bug1, count1, bug2, count2, bug3, count3):
     total = count1 + count2 + count3
     print("Bug Type     Count   Percentage")
     print("---------------------------------")
-    print(f"{bug1}{count1:>16}{calculate_percent(total, count1):>12.2f}%")
-    print(f"{bug2}{count2:>16}{calculate_percent(total, count2):>12.2f}%")
-    print(f"{bug3}{count3:>16}{calculate_percent(total, count3):>12.2f}%")
+    print(f"{bug1}{count1:>15}{calculate_percent(total, count1):>12f}%")
+    print(f"{bug2}{count2:>15}{calculate_percent(total, count2):>12f}%")
+    print(f"{bug3}{count3:>15}{calculate_percent(total, count3):>12f}%")
     print("---------------------------------")
     print("Bug Type      Count       100% ")
 
---
+++ unit 5/day 2/basic-itteration.py
@@ -0,0 +1,56 @@
+# list = [2,,4,5,,3,5,4,5,56,57,67,5,7,,54,56,,7,7,8,]    
+#     #print(list)
+#     #for element in list:
+#     #    print(element)
+
+#     #total = 0 
+#     # for el in list:
+#     #     total += el
+
+#     # print(total)
+#     # print(sum(list))  #the guessing way
+
+#     # print("positive even numbers")
+#     # for el in list:
+#     #     if el % 2 == 0:
+#     #         print(el)
+
+#     # smallest = [0]
+#     # for el in list:
+#     #     if el < smallest:
+#     #         smallest = el
+
+#     # print("get biggest")
+#     # biggest = list[0]
+#     # for el in list:
+#     #     if el > biggest:
+#     #         biggest = el
+#     # print(biggest)
+
+#     # print(min(list))
+#     # print(max(list))
+
+
+
+# list2 = ["monkey", "monkizzy", "monks", "monke", "ishaan"]
+# # biggest = list2[0]
+# # for el in list2:
+# #    if el < biggest:
+# #        biggest = el
+
+
+# # print(max(list2)) #monks
+# # print(min(list2)) #monke
+
+# # check=["A","a"]
+# # print(max(check))
+
+# # longest = 0
+# # for el in list2:
+# #     if len(longest) < len(el):
+# #         longest = el
+
+# # print(longest)
+
+# print(max(list2, key=len))
+# print(max(list2))
